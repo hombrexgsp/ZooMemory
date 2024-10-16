aa6c9637be9c99a93e9f4d1d99543ada59fe74b7
Gaby <gabriel.santana@globant.com>
2024-10-16 12:49:53 -0500

    👔 Added `AnimalMapper`
    So it changes between Entities and Value types more easily

---
+++ ZooMemoryAPI/Mapper/AnimalMapper.cs
@@ -0,0 +1,15 @@
+﻿using Riok.Mapperly.Abstractions;
+using ZooMemoryAPI.Domain.Animal;
+using ZooMemoryAPI.Domain.Entity;
+
+namespace ZooMemoryAPI.Mapper;
+
+[Mapper]
+public partial class AnimalMapper
+{
+    [MapperIgnoreTarget(nameof(AnimalEntity.Id))]
+    public partial AnimalEntity FromDto(Animal animal);
+    
+    [MapperIgnoreSource(nameof(AnimalEntity.Id))]
+    public partial Animal FromEntity(AnimalEntity entity);
+}
