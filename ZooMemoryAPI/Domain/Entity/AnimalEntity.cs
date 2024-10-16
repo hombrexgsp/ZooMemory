42751afd43334758abd432489878e829e4626969
Gaby <gabriel.santana@globant.com>
2024-10-16 12:45:54 -0500

    🏷️ Added `AnimalEntity` for handling Mongo EF collection

---
+++ ZooMemoryAPI/Domain/Entity/AnimalEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using ZooMemoryAPI.Domain.Conservation;
+
+namespace ZooMemoryAPI.Domain.Entity;
+
+public class AnimalEntity
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public ObjectId Id { get; set; }
+    
+    public Guid Uuid { get; set; }
+    
+    public required String Name { get; init; }
+    
+    public ConservationStatus Conservation { get; set; }
+    
+    public Int32 Population { get; set; }
+}
+    
+   
