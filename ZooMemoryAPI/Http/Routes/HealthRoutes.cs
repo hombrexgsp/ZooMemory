797d17a0e1f60a119cb7e6a52d55ba0029fc1437
Gaby <gabriel.santana@globant.com>
2024-10-16 12:43:58 -0500

    🩺 Added health check

---
+++ ZooMemoryAPI/Http/Routes/HealthRoutes.cs
@@ -0,0 +1,14 @@
+﻿using Carter;
+
+namespace ZooMemoryAPI.Http.Routes;
+
+public class HealthRoutes() : CarterModule("/health")
+{
+    public override void AddRoutes(IEndpointRouteBuilder builder)
+    {
+        builder
+            .MapGet("/{name}", async (String name) => 
+                await Task.Run(() => $"Hello from service, {name}"));
+    }
+    
+}
