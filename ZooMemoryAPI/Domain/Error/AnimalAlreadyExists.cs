50eddf7411cf783d942748a2caff29a48d83dc4b
Gaby <gabriel.santana@globant.com>
2024-10-16 12:45:23 -0500

    🏷️ Added exception types
    Maybe migrate to monads later?

---
+++ ZooMemoryAPI/Domain/Error/AnimalAlreadyExists.cs
@@ -0,0 +1,4 @@
+﻿namespace ZooMemoryAPI.Domain.Error;
+
+public class AnimalAlreadyExists(Guid uuid) : Exception($"Animal with id {uuid} already exists");
+    
---
+++ ZooMemoryAPI/Domain/Error/AnimalNotFound.cs
@@ -0,0 +1,3 @@
+﻿namespace ZooMemoryAPI.Domain.Error;
+
+public class AnimalNotFound(Guid uuid) : Exception($"Animal with id {uuid} not found");
