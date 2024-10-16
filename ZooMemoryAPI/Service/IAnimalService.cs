4176137a87a904b587b2709d14002cda21bf946d
Gaby <gabriel.santana@globant.com>
2024-10-16 12:48:10 -0500

    👔 Added `AnimalService`
    Handles nullability and errors
    
    Possible refactor: Change exceptions for monads to improve performance

---
+++ ZooMemoryAPI/Service/IAnimalService.cs
@@ -0,0 +1,11 @@
+﻿using ZooMemoryAPI.Domain.Animal;
+
+namespace ZooMemoryAPI.Service;
+
+public interface IAnimalService
+{
+    Task<IEnumerable<Animal>> GetAnimalsAsync();
+    Task<Animal> GetAnimalAsync(Guid id);
+    Task<Animal> AddAnimalAsync(CreateAnimal createAnimal);
+    Task<Boolean> DeleteAnimalAsync(Guid id);
+}
--- ZooMemoryAPI/Service/Implementations/AnimalService.cs
+++ ZooMemoryAPI/Service/Implementations/AnimalService.cs
@@ -1,6 +1,38 @@
-﻿namespace ZooMemoryAPI.Service.Implementations;
+﻿using ZooMemoryAPI.Domain.Animal;
+using ZooMemoryAPI.Mapper;
+using ZooMemoryAPI.Repository;
 
-public class AnimalService
+using ZooMemoryAPI.Domain.Entity;
+using ZooMemoryAPI.Domain.Error;
+
+namespace ZooMemoryAPI.Service.Implementations;
+
+public class AnimalService(
+    IAnimalRepository animalRepository,
+    AnimalMapper mapper) : IAnimalService
 {
-    
+    public async Task<IEnumerable<Animal>> GetAnimalsAsync()
+    {
+        var animalList = await animalRepository.GetAllAsync();
+        return animalList.Select(mapper.FromEntity);
+    }
+
+    public async Task<Animal> GetAnimalAsync(Guid uuid) =>
+        await animalRepository.GetAsync(uuid)
+            .Bind(animal =>
+                animal is null
+                    ? TaskFail<AnimalEntity>(new AnimalNotFound(uuid))
+                    : TaskSucc(animal))
+            .Select(mapper.FromEntity);
+
+    public async Task<Animal> AddAnimalAsync(CreateAnimal createAnimal) =>
+        await animalRepository .CreateAsync(mapper.FromDto(createAnimal.ToAnimal()))
+            .Select(mapper.FromEntity);
+
+    public async Task<Boolean> DeleteAnimalAsync(Guid uuid) =>
+        await animalRepository.GetAsync(uuid)
+            .Bind(animal =>
+                animal is null
+                    ? TaskFail<Boolean>(new AnimalNotFound(uuid))
+                    : animalRepository.DeleteAsync(uuid));
 }
